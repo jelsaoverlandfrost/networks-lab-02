421bf31f50815df17b37b8ebbc1b561e0462c89b
Frost Jack <jelsaoverlandfrost@gmail.com>
2018-09-24 12:37:49 +0800

    Default Changelist

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, Response
+from functools import wraps
 import json
 
 app = Flask(__name__)
 
-
 json_file = open("pillar.json", 'r')
-Pillar = json.load(json_file)
-Pillar = Pillar["pillar"]
+pillar_json = json.load(json_file)
+pillar_json = pillar_json["pillar"]
 
 
 # Root
@@ -15,15 +15,40 @@ def api_root():
     return 'Welcome to SUTD CourseList!'
 
 
+# Authentication
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    users = {'admin': 'password', 'user1': "123456", 'user2': '234567'}
+    if username in users.keys():
+        if password == users[username]:
+            return True
+    else:
+        return False
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
+# pillar
 @app.route('/pillar', methods=['GET'])
 def pillar():
-    return jsonify({'name': Pillar})
+    return jsonify({'name': pillar_json})
 
 
 @app.route('/pillar/<pillar_name>', methods=['GET'])
+@requires_auth
 def get_pillar(pillar_name):
     pillar = []
-    for pillars in Pillar:
+    for pillars in pillar_json:
         if pillars['name'] == pillar_name:
             pillar = pillars
     if len(pillar) == 0:
@@ -31,10 +56,32 @@ def get_pillar(pillar_name):
     return jsonify({'pillar': pillar})
 
 
+@app.route('/pillar/<pillar_name>/info/<mime_type>', methods=['GET'])
+@requires_auth
+# get pillar information as json file or txt file
+def get_text_with_format(pillar_name, mime_type):
+    pillar = []
+    for pillars in pillar_json:
+        if pillars["name"] == pillar_name:
+            pillar = pillars
+        if len(pillar) == 0:
+            abort(404)
+        else:
+            track_info = ""
+            if mime_type == "info.json":
+                return jsonify({'pillar': pillar})
+            elif mime_type == "info.txt":
+                for tracks in pillar['track']:
+                    track_info = track_info + tracks['tname'] + ", "
+                return "Your pillar is " + pillar_name + ", and it includes tracks: " + track_info
+            else:
+                abort(404)
+
+
 @app.route('/pillar/<pillar_name>/<track_name>', methods=['GET'])
 def get_track(pillar_name, track_name):
     pillar = []
-    for pillars in Pillar:
+    for pillars in pillar_json:
         if pillars['name'] == pillar_name:
             pillar = pillars
     if len(pillar) == 0:
@@ -50,31 +97,41 @@ def get_track(pillar_name, track_name):
 
 
 @app.route('/pillar', methods=['POST'])
+@requires_auth
 def create_pillar():
     if not request.json or not 'name' in request.json:
         abort(400)
     pillar = {
         "name": request.json['name'],
-        "id": Pillar[-1]['id'] + 1
+        "id": pillar_json[-1]['id'] + 1
     }
-    Pillar.append(pillar)
+    pillar_json.append(pillar)
     with open("pillar.json", "w") as f:
-        json.dump({"pillar": Pillar}, f)
+        json.dump({"pillar": pillar_json}, f)
     return jsonify({'new_pillar': pillar}), 201
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 
 @app.route('/pillar/<pillar_name>', methods=['DELETE'])
 def delete_pillar(pillar_name):
     pillar = []
-    for pillars in Pillar:
+    for pillars in pillar_json:
         if pillars['name'] == pillar_name:
             pillar = pillars
     if len(pillar) == 0:
         abort(404)
-    Pillar.remove(pillar)
+    pillar_json.remove(pillar)
     with open("pillar.json", "w") as f:
-        json.dump({"pillar": Pillar}, f)
+        json.dump({"pillar": pillar_json}, f)
     return jsonify({'pillar': pillar})
+
 
 if __name__ == '__main__':
     app.run()
---
+++ client.py
@@ -0,0 +1,42 @@
+import requests
+
+print "--- Get connection to the API ---"
+r = requests.get("http://localhost:5000/")
+print r.headers
+print r.text
+
+print "--- Get information for all the pillars ---"
+r = requests.get("http://localhost:5000/pillar")
+print r.headers
+print r.text
+
+print "--- Get information for ISTD tracks, after authentication ---"
+r = requests.get("http://localhost:5000/pillar/ISTD", auth=('admin', 'password'))
+print r.headers
+print r.text
+
+print "--- Get json type of the ISTD information ---"
+r = requests.get("http://localhost:5000/pillar/ISTD/info/info.json", auth=('admin', 'password'))
+print r.headers
+print r.text
+
+print "Get text type of the ISTD information"
+r = requests.get("http://localhost:5000/pillar/ISTD/info/info.txt", auth=('admin', 'password'))
+print r.headers
+print r.text
+
+print "Get course information for Cyber Security Track"
+r = requests.get("http://localhost:5000/pillar/ISTD/Cyber%20Security")
+print r.headers
+print r.text
+
+print "Add a new pillar using the post method"
+headers = {'Content-type': 'application/json'}
+r = requests.post("http://localhost:5000/pillar", json={'name': 'EPD'}, auth=('user1', '123456'))
+print r.headers
+print r.text
+
+print "Delete a pillar using the post method"
+r = requests.delete("http://localhost:5000/pillar/EPD")
+print r.headers
+print r.text
